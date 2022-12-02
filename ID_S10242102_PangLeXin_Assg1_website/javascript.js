978d25e14fa8349f3cf5a99e7bd5b708a7c495d4
Pang Le Xin <s10242102@connect.np.edu.sg>
2022-12-02 23:40:33 +0800

    video walkthrough demo

--- ID_S10242102_PangLeXin_Assg1_website/javascript.js
+++ ID_S10242102_PangLeXin_Assg1_website/javascript.js
@@ -1,8 +1,11 @@
 let btnSubmit = document.getElementById('submitBtn');
 let msg ="Thank you for contacting us! You will receive an email in 3-4 working days.";
-      
+   
+
 btnSubmit.addEventListener('click', function(e){
+
   e.preventDefault();
   document.getElementById('userdetails').innerHTML = msg;
  
 });
+
