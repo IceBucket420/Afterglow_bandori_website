646411236898d1d2ba2893d2edbca12ebab9abda
Pang Le Xin <s10242102@connect.np.edu.sg>
2022-12-02 10:20:15 +0800

    contact for updates

--- Code/javascript.js
+++ Code/javascript.js
@@ -1,1 +1,8 @@
-// will put somethings soon
+let btnSubmit = document.getElementById('submitBtn');
+let msg ="Thank you for contacting us! You will receive an email in 3-4 working days.";
+      
+btnSubmit.addEventListener('click', function(e){
+  e.preventDefault();
+  document.getElementById('userdetails').innerHTML = msg;
+ 
+});
